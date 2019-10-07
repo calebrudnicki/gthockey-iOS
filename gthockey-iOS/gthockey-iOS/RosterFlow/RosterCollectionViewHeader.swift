e7967b49a826686e27055e8ed355442c6d319c7c
Caleb Rudnicki <calebrudnicki@gmail.com>
2019-10-07 16:11:02 -0400

    Started the roster page (#46)
    More work on the roster collection view
    
    Another small change
    
    Created sections for the roster
    
    Delete LICENSE
    
    Delete README.md
    
    Delete TOHeaderImageView.h
    
    Delete TOHeaderImageView.m
    
    Delete TONavigationBar.h
    
    Delete TONavigationBar.m
    
    Delete TONavigationBar.xcconfig
    
    Delete TONavigationBar-Info.plist
    
    Delete TONavigationBar-dummy.m
    
    Delete TONavigationBar-prefix.pch
    
    Delete TONavigationBar.modulemap
    
    Delete TONavigationBar-umbrella.h

---
+++ gthockey-iOS/gthockey-iOS/RosterFlow/RosterCollectionViewHeader.swift
@@ -0,0 +1,47 @@
+//
+//  RosterCollectionViewHeader.swift
+//  gthockey-iOS
+//
+//  Created by Caleb Rudnicki on 10/7/19.
+//  Copyright © 2019 Caleb Rudnicki. All rights reserved.
+//
+
+import UIKit
+
+class RosterCollectionViewHeader: UICollectionReusableView {
+
+    private let titleLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.font = UIFont(name:"Helvetica Neue", size: 24.0)
+        titleLabel.numberOfLines = 1
+        titleLabel.textAlignment = .center
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        return titleLabel
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        addSubview(titleLabel)
+
+        updateConstraints()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func updateConstraints() {
+        super.updateConstraints()
+
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12.0),
+            titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -12.0)
+        ])
+    }
+
+    public func set(with title: String) {
+        titleLabel.text = title
+    }
+
+}
