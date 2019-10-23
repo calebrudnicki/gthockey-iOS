ce80a5b86d7d7a40ed0fdfa0168072296a51b338
Caleb Rudnicki <calebrudnicki@gmail.com>
2019-10-23 15:51:06 -0400

    Added an intermediate step for HTML enabled articles (#71)

--- gthockey-iOS/gthockey-iOS/HomeFlow/HomeDetailViewController.swift
+++ gthockey-iOS/gthockey-iOS/HomeFlow/HomeDetailViewController.swift
@@ -172,11 +172,31 @@ class HomeDetailViewController: UIViewController {
         formatter.dateStyle = .long
         dateLabel.text = formatter.string(from: news.getDate())
 
-        bodyLabel.text = news.getContent()
+        bodyLabel.text = news.getContent().htmlToString
     }
 
     @objc private func closeButtonTapped() {
         dismiss(animated: true, completion: nil)
     }
 
+}
+
+extension String {
+    var htmlToAttributedString: NSAttributedString? {
+        guard let data = data(using: .utf8) else { return NSAttributedString() }
+
+        do {
+            return try NSAttributedString(data: data,
+                                          options: [.documentType: NSAttributedString.DocumentType.html,
+                                                    .characterEncoding: String.Encoding.utf8.rawValue],
+                                          documentAttributes: nil)
+        } catch {
+               return NSAttributedString()
+        }
+        return NSAttributedString()
+    }
+
+    var htmlToString: String {
+        return htmlToAttributedString?.string ?? ""
+    }
 }
