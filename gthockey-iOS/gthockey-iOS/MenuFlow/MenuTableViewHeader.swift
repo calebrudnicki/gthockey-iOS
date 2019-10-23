4b6e71ec3787b7ec877b9c1299ce5dde7a468d6c
Caleb Rudnicki <calebrudnicki@lawn-143-215-54-65.lawn.gatech.edu>
2019-10-23 16:08:02 -0400

    Fixed spacing on menu table header view

--- gthockey-iOS/gthockey-iOS/MenuFlow/MenuTableViewHeader.swift
+++ gthockey-iOS/gthockey-iOS/MenuFlow/MenuTableViewHeader.swift
@@ -38,7 +38,7 @@ class MenuTableViewHeader: UIView {
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: topAnchor),
             titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20.0),
-            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12.0),
             titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor)
         ])
     }
