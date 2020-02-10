3430c247a1389641c9111c2d0a5267d14ea3efe9
Caleb Rudnicki <calebrudnicki@Calebs-MBP.fios-router.home>
2020-02-09 19:30:48 -0500

    Created ability to send notification

---
+++ gthockey-iOS/gthockey-iOS/PushNotificationSender.swift
@@ -0,0 +1,37 @@
+//
+//  PushNotificationSender.swift
+//  gthockey-iOS
+//
+//  Created by Caleb Rudnicki on 2/9/20.
+//  Copyright © 2020 Caleb Rudnicki. All rights reserved.
+//
+
+import Foundation
+
+class PushNotificationSender {
+    func sendPushNotification(to token: String, title: String, body: String) {
+        let urlString = "https://fcm.googleapis.com/fcm/send"
+        let url = NSURL(string: urlString)!
+        let paramString: [String : Any] = ["to" : token,
+                                           "notification" : ["title" : title, "body" : body],
+                                           "data" : ["user" : "test_id"]
+        ]
+        let request = NSMutableURLRequest(url: url as URL)
+        request.httpMethod = "POST"
+        request.httpBody = try? JSONSerialization.data(withJSONObject:paramString, options: [.prettyPrinted])
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("key=AAAAUgiuQUM:APA91bFZvhw4nZzZUb32_E_hr3SI1seLNLxHiYGa8yZjwOLUrhSdViK-Sb4bwwveR9bXq-1TR2_xFCNXUoFJajPp6YcKLDGpADoq7BlBmvIU-mGxlH_SjyX3mpZ0jajFzEf0EoiZgd3w", forHTTPHeaderField: "Authorization")
+        let task =  URLSession.shared.dataTask(with: request as URLRequest)  { (data, response, error) in
+            do {
+                if let jsonData = data {
+                    if let jsonDataDict  = try JSONSerialization.jsonObject(with: jsonData, options: JSONSerialization.ReadingOptions.allowFragments) as? [String: AnyObject] {
+                        NSLog("Received data:\n\(jsonDataDict))")
+                    }
+                }
+            } catch let err as NSError {
+                print(err.debugDescription)
+            }
+        }
+        task.resume()
+    }
+}
