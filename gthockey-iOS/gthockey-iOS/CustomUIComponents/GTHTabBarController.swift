671eaefd5ea550904828df267ae31c0180e8dca1
Caleb Rudnicki <calebrudnicki@gmail.com>
2020-05-05 15:27:20 -0700

    Created tab bar

---
+++ gthockey-iOS/gthockey-iOS/CustomUIComponents/GTHTabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  GTHTabBarController.swift
+//  gthockey-iOS
+//
+//  Created by Caleb Rudnicki on 5/5/20.
+//  Copyright © 2020 Caleb Rudnicki. All rights reserved.
+//
+
+import UIKit
+
+class GTHTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tabBar.tintColor = .techNavy
+    }
+
+}
